7bfbdc5cd6292d316c50d147d912396ab17e1a54
배민경 <bmk0703@naver.com>
2025-02-06 20:17:41 +0900

    Fix DateField(auto_now_add)

--- SpartaMarkets/models.py
+++ SpartaMarkets/models.py
@@ -5,6 +5,6 @@ class MarketModel(models.Model):
     MarketZone = models.TextField(max_length=300) # 지점 지역
     MarketManager = models.TextField(max_length=50) # 지점장 이름
     MarketNumber = models.TextField(max_length=50) # 지점 번호
-    MarketOpeningDate = models.DateField() # 개점 날짜
+    MarketOpeningDate = models.DateField(auto_now_add=True) # 개점 날짜
     MarketEmployees = models.IntegerField(max_length=50) # 직원 수
     
