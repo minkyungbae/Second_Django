13dc7dca98f4438fd006c9845a4dce15e1b9d8af
배민경 <bmk0703@naver.com>
2025-02-06 19:53:58 +0900

    Add ProductModel

--- Products/models.py
+++ Products/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from SpartaMarkets.models import MarketModel
 
-# Create your models here.
+class ProductModel(models.Model):
+    ProductID = models.CharField(max_length=100, primary_key=True) # 상품 ID
+    ProductName = models.CharField(max_length=100) # 상품 이름
+    ProductPrice = models.IntegerField() # 상품 가격
+    Productstock = models.IntegerField() # 상품 재고
+    ProductZoneID = models.ForeignKey(
+        MarketModel, on_delete=models.CASCADE, related_name="product") # 지점 ID
+    
+    
