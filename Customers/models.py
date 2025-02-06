aa62856ec046572ea81c24cfd84bc08cb2833910
배민경 <bmk0703@naver.com>
2025-02-06 19:41:27 +0900

    Add CustomerModel

--- Customers/models.py
+++ Customers/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class CustomerModel(models.Model):
+    CustomerID = models.CharField(max_length=100) # 고객 ID
+    CustomerName = models.CharField(max_length=50) # 고괙 이름
+    CustomerRegDate = models.DateField() # 등록일
+    CustomerNumber = models.CharField(max_length=50) # 고객 번호
+    
+    
