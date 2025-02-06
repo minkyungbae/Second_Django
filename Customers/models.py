e502ec11ac71de4c02f20b2de6def08c5f392dff
배민경 <bmk0703@naver.com>
2025-02-06 20:18:05 +0900

    Fix DateField(auto_now_add)

--- Customers/models.py
+++ Customers/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class CustomerModel(models.Model):
     CustomerID = models.CharField(max_length=100, primary_key=True) # 고객 ID
     CustomerName = models.CharField(max_length=50) # 고괙 이름
-    CustomerRegDate = models.DateField() # 등록일
+    CustomerRegDate = models.DateField(auto_now_add=True) # 등록일
     CustomerNumber = models.CharField(max_length=50) # 고객 번호
     
     
