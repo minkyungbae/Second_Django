863a54960ffd1a2f9f8c842b82e870a058e2442d
배민경 <bmk0703@naver.com>
2025-02-06 20:37:12 +0900

    Fix 대문자를 소문자로

--- Products/models.py
+++ Products/models.py
@@ -2,11 +2,11 @@ from django.db import models
 from SpartaMarkets.models import MarketModel
 
 class ProductModel(models.Model):
-    ProductID = models.CharField(max_length=100, primary_key=True) # 상품 ID
-    ProductName = models.CharField(max_length=100) # 상품 이름
-    ProductPrice = models.IntegerField() # 상품 가격
-    Productstock = models.IntegerField() # 상품 재고
-    ProductZoneID = models.ForeignKey(
+    product_id = models.CharField(max_length=100, primary_key=True) # 상품 ID
+    product_name = models.CharField(max_length=100) # 상품 이름
+    product_price = models.IntegerField() # 상품 가격
+    product_stock = models.IntegerField() # 상품 재고
+    product_zone_id = models.ForeignKey(
         MarketModel, on_delete=models.CASCADE, related_name="product") # 지점 ID
     
     
