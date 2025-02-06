a971667b2344acff062b2a24ade7d728c57701c3
배민경 <bmk0703@naver.com>
2025-02-06 19:23:55 +0900

    Add my New Apps

--- Second_Django_pjt/settings.py
+++ Second_Django_pjt/settings.py
@@ -38,6 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # My App
+    'SpartaMarkets',
+    'Products',
+    'Customers',
 ]
 
 MIDDLEWARE = [
