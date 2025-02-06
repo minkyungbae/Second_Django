ed908e1e8054a569694675f6468d93b1fa984fe9
배민경 <bmk0703@naver.com>
2025-02-06 20:36:38 +0900

    Fix 대문자를 소문자로

--- SpartaMarkets/models.py
+++ SpartaMarkets/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class MarketModel(models.Model):
-    MarketID = models.CharField(max_length=50) # 지점ID
-    MarketZone = models.TextField(max_length=300) # 지점 지역
-    MarketManager = models.TextField(max_length=50) # 지점장 이름
-    MarketNumber = models.TextField(max_length=50) # 지점 번호
-    MarketOpeningDate = models.DateField(auto_now_add=True) # 개점 날짜
-    MarketEmployees = models.IntegerField(max_length=50) # 직원 수
+    market_id = models.CharField(max_length=50)  # 지점 ID
+    market_zone = models.CharField(max_length=300)  # 지점 지역
+    market_manager = models.CharField(max_length=50)  # 지점장 이름
+    market_number = models.CharField(max_length=50)  # 지점 전화번호
+    market_opening_date = models.DateField(auto_now_add=True)  # 개점 날짜
+    market_employees = models.IntegerField()  # 직원 수
     
