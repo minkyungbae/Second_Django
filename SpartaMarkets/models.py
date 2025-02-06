dd74c85dc6200ef6459b4119a0cb1a475d4115ef
배민경 <bmk0703@naver.com>
2025-02-06 19:34:26 +0900

    Add MarketModel

--- SpartaMarkets/models.py
+++ SpartaMarkets/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class MarketModel(models.Model):
+    MarketID = models.CharField(max_length=50) # 지점ID
+    MarketZone = models.TextField(max_length=300) # 지점 지역
+    MarketManager = models.TextField(max_length=50) # 지점장 이름
+    MarketNumber = models.TextField(max_length=50) # 지점 번호
+    MarketOpeningDate = models.DateField() # 개점 날짜
+    MarketEmployees = models.IntegerField(max_length=50) # 직원 수
+    
