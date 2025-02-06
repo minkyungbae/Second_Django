64bfddb40e224e83197e33b3358b3d5629ad8770
배민경 <bmk0703@naver.com>
2025-02-06 20:37:30 +0900

    Fix 대문자를 소문자로

--- Customers/models.py
+++ Customers/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class CustomerModel(models.Model):
-    CustomerID = models.CharField(max_length=100, primary_key=True) # 고객 ID
-    CustomerName = models.CharField(max_length=50) # 고괙 이름
-    CustomerRegDate = models.DateField(auto_now_add=True) # 등록일
-    CustomerNumber = models.CharField(max_length=50) # 고객 번호
+    customer_id = models.CharField(max_length=100, primary_key=True) # 고객 ID
+    customer_name = models.CharField(max_length=50) # 고괙 이름
+    customer_regdate = models.DateField(auto_now_add=True) # 등록일
+    customer_number = models.CharField(max_length=50) # 고객 번호
     
     
