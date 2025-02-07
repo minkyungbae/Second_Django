06cbc8d11434309dcf8c820f283fc8b16013cd3b
배민경 <bmk0703@naver.com>
2025-02-07 19:35:09 +0900

    Fix forms.ModelForm

--- SpartaMarkets/forms.py
+++ SpartaMarkets/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from .models import MarketModel
 
-class MarketModelForm(forms.modelform):
+class MarketModelForm(forms.ModelForm):
     class Meta:
         model = MarketModel
         field = [
