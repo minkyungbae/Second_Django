5d7245c888fcd5514ac29498495f7ef1200f22a4
배민경 <bmk0703@naver.com>
2025-02-10 19:23:54 +0900

    Add MarketInfoAPIView <int:marked_id> url

--- SpartaMarkets/urls.py
+++ SpartaMarkets/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.MarketInfoAPIView.as_view()), # market 정보 가져오기
+    path('<int:market_id>/', views.MarketInfoAPIView.as_view()), # 특정 market 정보 가져오기
     
 ]
