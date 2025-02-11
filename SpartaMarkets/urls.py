53f7968982c60b6bc29cfb928257769e039e6bd3
배민경 <bmk0703@naver.com>
2025-02-11 20:24:35 +0900

    Add delete, update, detail url

--- SpartaMarkets/urls.py
+++ SpartaMarkets/urls.py
@@ -4,6 +4,8 @@ from . import views
 app_name = "spartamarket"
 urlpatterns = [
     path('', views.MarketInfoView.as_view(), name="market-list"), # market list
-    path('/create/', views.MarketCreateView.as_view(), name="create-market"), # 마켓 새로 입력하기
-    path('<int:market_id/', views.MarketInfoDetailView.as_view, name="detail") # 특정 id 게시물 목록 조회, 삭제 및 수정
+    path('create/', views.MarketCreateView.as_view(), name="create-market"), # 마켓 새로 입력하기
+    path('detail/<int:market_id/', views.MarketInfoDetailView.as_view, name="detail"), # 특정 id 게시물 보기
+    path('delete/<int:market_id/', views.MarketDeleteView.as_view(), name='delete'), # 마켓 삭제하기
+    path('update/<int:market_id>', views.MarketPutView.as_view(), name="update"), # 수정하기
 ]
