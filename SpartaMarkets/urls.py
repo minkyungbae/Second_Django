f78066e3acb7fa7a347cb07583e6997a746cf96f
배민경 <bmk0703@naver.com>
2025-02-11 19:43:56 +0900

    Add MarketCreateView url

--- SpartaMarkets/urls.py
+++ SpartaMarkets/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 app_name = "spartamarket"
 urlpatterns = [
-    path('', views.MarketInfoView.as_view(), name="market_list"), # market list
+    path('', views.MarketInfoView.as_view(), name="market-list"), # market list
+    path('/create/', views.MarketCreateView.as_view(), name="create-market"), # 마켓 새로 입력하기
     path('<int:market_id/', views.MarketInfoDetailView.as_view, name="detail") # 특정 id 게시물 목록 조회, 삭제 및 수정
 ]
