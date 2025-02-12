6c03d346adf8373d3ad1bf5b780f49d736644c1f
배민경 <bmk0703@naver.com>
2025-02-12 11:09:25 +0900

    Add DateField(blank=Ture)

--- SpartaMarkets/models.py
+++ SpartaMarkets/models.py
@@ -5,6 +5,6 @@ class MarketModel(models.Model):
     market_zone = models.CharField(max_length=300)  # 지점 지역
     market_manager = models.CharField(max_length=50)  # 지점장 이름
     market_number = models.CharField(max_length=50)  # 지점 전화번호
-    market_opening_date = models.DateField(auto_now_add=True)  # 개점 날짜
+    market_opening_date = models.DateField(auto_now_add=True, blank=True)  # 개점 날짜
     market_employees = models.IntegerField()  # 직원 수
     
