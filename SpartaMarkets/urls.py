3f8b3a580dbf0e9b7e2e82fb8526ebc387f9e38b
배민경 <bmk0703@naver.com>
2025-02-13 22:07:13 +0900

    Update

--- SpartaMarkets/urls.py
+++ SpartaMarkets/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('create/', views.MarketCreateView.as_view(), name="create"), # 마켓 새로 입력하기
     path('detail/<int:market_id>/', views.MarketInfoDetailView.as_view(), name="detail"), # 특정 id 게시물 보기
     path('delete/<int:market_id>/', views.MarketDeleteView.as_view(), name='delete'), # 마켓 삭제하기
-    path('update/<int:market_id>', views.MarketPutView.as_view(), name="update"), # 수정하기
+    path('update/<int:market_id>/', views.MarketPutView.as_view(), name="update"), # 수정하기
 ]
