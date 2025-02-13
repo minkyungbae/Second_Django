74bbfbdbe542d2bd1d6b0eb580deaa18891277e5
배민경 <bmk0703@naver.com>
2025-02-13 22:11:59 +0900

    Add class profile, detail, create

--- Customers/views.py
+++ Customers/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views import View
+from django.urls import reverse
 
-# Create your views here.
+from .models import CustomerModel
+from .forms import CustomerModelForm
+
+# 프로필 보기
+class CustomerProfileListView(View):
+    def get(self, request):
+        customers = CustomerModel.objects.all()
+        context = {"customers":customers}
+        return render(request, "Customers/customerList.html", context)
+    
+# 프로필 디테일
+class CustomerDetailView(View):
+    def get(self, request, customer_id):
+        customer = get_object_or_404(CustomerModel, customer_id=customer_id)
+        return render(request, "Customers/detail.html", {"customer":customer})
+        
+# customer 정보 가입하기
+class CustomerCreateView(View):
+    def get(self, request):
+        form = CustomerModelForm()
+        return render(request, "Customers/create.html", {"form":form})
+    
+    def post(self, request):
+        form = CustomerModelForm(request.POST)
+        if form.is_valid():
+            customer = form.save()
+            return redirect(reverse("customer:detail", args=[customer.customer_id]))
+        return render(request, "Customers/create.html", {"form":form})
+        
+        
