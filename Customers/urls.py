4384556a27edec152e94d864a1e16b22a46bd507
배민경 <bmk0703@naver.com>
2025-02-13 22:38:27 +0900

    Add create url

--- Customers/urls.py
+++ Customers/urls.py
@@ -5,6 +5,5 @@ app_name = "customer"
 urlpatterns = [
     path("customerlist/", views.CustomerListView.as_view(), name="customerlist"), # 프로필 리스트 보기
     path("detail/<str:customer_id>/", views.CustomerDetailView.as_view(), name="detail"), # 프로필 디테일
-     
-
+    path("create/", views.CustomerCreateView.as_view(), name="create"), # 생성하기
 ]
