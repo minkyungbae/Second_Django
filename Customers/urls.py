7464ebee1da456bc1be890c11b5e68000a7bbcef
배민경 <bmk0703@naver.com>
2025-02-13 22:10:55 +0900

    Add customerlist, detail url

--- Customers/urls.py
+++ Customers/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from . import views
 
+app_name = "customer"
 urlpatterns = [
-    
+    path("customerlist/", views.CustomerProfileListView.as_view(), name="customerlist"), # 프로필 리스트 보기
+    path("detail/<str:customer_id>/", views.CustomerDetailView.as_view(), name="detail"), # 프로필 디테일
+     
+
 ]
