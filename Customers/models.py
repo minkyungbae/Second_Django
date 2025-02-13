7806cc8a85ee6b588a41d344dc195111a79619e4
배민경 <bmk0703@naver.com>
2025-02-13 22:39:31 +0900

    Fix code, regdate(null=True)

--- Customers/models.py
+++ Customers/models.py
@@ -3,5 +3,5 @@ from django.db import models
 class CustomerModel(models.Model):
     customer_id = models.CharField(max_length=100, primary_key=True) # 고객 ID
     customer_name = models.CharField(max_length=50) # 고괙 이름
-    customer_regdate = models.DateField(auto_now_add=True) # 등록일
+    customer_regdate = models.DateField(auto_now_add=True, null=True) # 등록일
     customer_number = models.CharField(max_length=50) # 고객 번호
