2d6b14c6e95f0963995d7df9f7b87ef6e7c8dd78
배민경 <bmk0703@naver.com>
2025-02-14 00:54:39 +0900

    Fix code, add Update & deleteView

--- Customers/views.py
+++ Customers/views.py
@@ -3,12 +3,14 @@ from django.views import View
 from django.urls import reverse
 
 from .models import CustomerModel
-from .forms import CustomerModelForm
+from .forms import CustomerModelForm, CustomerUpdateForm
 
 # 프로필 보기
 class CustomerListView(View):
     def get(self, request):
         customers = CustomerModel.objects.all()
+        for customer in customers:
+            print(f"Customer ID : {customer.customer_id}")
         context = {"customers":customers}
         return render(request, "Customers/customerList.html", context)
     
@@ -31,4 +33,24 @@ class CustomerCreateView(View):
             return redirect(reverse("customer:detail", args=[customer.customer_id]))
         return render(request, "Customers/create.html", {"form":form})
         
-        
+# 수정하기
+class CustomerUpdateView(View):
+    def get(self, request, customer_id):
+        customer = get_object_or_404(CustomerModel, customer_id=customer_id)
+        form = CustomerUpdateForm(instance=customer)
+        return render(request, "Customers/update.html", {"form":form, "customer":customer})
+    
+    def post(self, request, customer_id):
+        customer = get_object_or_404(CustomerModel, customer_id=customer_id)
+        form = CustomerUpdateForm(request.POST, instance=customer)
+        if form.is_valid():
+            form.save()
+            return redirect("customer:detail", customer_id=customer_id)
+        return render(request, "Customers/update.html", {"form":form, "customer":customer})
+    
+# 삭제하기
+class CustomerDeleteView(View):
+    def post(self, request, customer_id):
+        customer = get_object_or_404(CustomerModel, customer_id=customer_id)
+        customer.delete()
+        return redirect(reverse("customer:customerlist"))
