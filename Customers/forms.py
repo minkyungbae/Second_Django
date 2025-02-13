c4a8bc00f6191ec5c13dfad8f621fe0201811192
배민경 <bmk0703@naver.com>
2025-02-13 22:38:58 +0900

    Fix code, add exclude

--- Customers/forms.py
+++ Customers/forms.py
@@ -2,10 +2,11 @@ from django import forms
 from .models import CustomerModel
 
 class CustomerModelForm(forms.ModelForm):
-    model = CustomerModel
-    fields = [
-        "customer_id",
-        "customer_name",
-        "customer_regdate",
-        "customer_number",
-    ]
+    class Meta:
+        model = CustomerModel
+        fields = [
+            "customer_id",
+            "customer_name",
+            "customer_number",
+            ]
+        exclude = ("customer_regdate",)
