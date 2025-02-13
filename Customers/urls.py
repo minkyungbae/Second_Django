57e20fe06e9203706f44adf84ec73697bbbdbea7
배민경 <bmk0703@naver.com>
2025-02-13 22:12:59 +0900

    Fix code, CustomerProfileListView-> CustomerListView

--- Customers/urls.py
+++ Customers/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = "customer"
 urlpatterns = [
-    path("customerlist/", views.CustomerProfileListView.as_view(), name="customerlist"), # 프로필 리스트 보기
+    path("customerlist/", views.CustomerListView.as_view(), name="customerlist"), # 프로필 리스트 보기
     path("detail/<str:customer_id>/", views.CustomerDetailView.as_view(), name="detail"), # 프로필 디테일
      
 
