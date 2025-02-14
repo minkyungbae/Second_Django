32ad5e00093a8a3cfe7f27214e02ba03b64e9b8c
배민경 <bmk0703@naver.com>
2025-02-14 19:07:23 +0900

    Fix App

--- Second_Django_pjt/settings.py
+++ Second_Django_pjt/settings.py
@@ -38,13 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
-    # etc
-    # 'rest_framework',
-    
+
     # My App
     'SpartaMarkets',
-    'Products',
     'Customers',
 ]
 
